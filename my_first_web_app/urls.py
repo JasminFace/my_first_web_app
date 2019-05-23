e193bf72fdb64a8b05047c02565f2a52266d6652
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-23 17:11:15 -0400

    moved functions from urls.py to views.py

--- my_first_web_app/urls.py
+++ my_first_web_app/urls.py
@@ -13,41 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from random import randint
-from django.http import HttpResponse
-from django.shortcuts import render
 from django.urls import path
-
-def home_page(request):
-    response = render(request, 'index.html')
-    return HttpResponse(response)
-
-def portfolio(request):
-    image_urls = []
-    for i in range(5):
-        random_number = randint(0,100)
-        image_urls.append("https://picsum.photos/400/600/?image={}".format(random_number))
-
-    context = {'gallery_images': image_urls}
-    response = render(request, 'gallery.html', context)
-    return HttpResponse(response)
-
-def about_me(request):
-    context = {
-        'skills': ['Dependable', 'Problem solving', 'Persistent', 'Creative'],
-        'interests': ['Fitness', 'Eating', 'Gunpla', 'You ;)']
-    }
-    response = render(request, 'about_me.html', context)
-    return HttpResponse(response)
-
-def favourites(request):
-    context = {
-        'fave_links': {'Bitmaker':'https://bitmaker.co/',
-        'Bitmaker Web Developement': 'https://bitmaker.co/courses/web-development',
-        'LMGTFY':'https://lmgtfy.com/'}
-    }
-    response = render(request, 'favourites.html', context)
-    return HttpResponse(response)
+from my_first_web_app.views import home_page, portfolio, about_me, favourites
 
 urlpatterns = [
 path('home/', home_page),
---
+++ my_first_web_app/views.py
@@ -0,0 +1,34 @@
+from random import randint
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_page(request):
+    response = render(request, 'index.html')
+    return HttpResponse(response)
+
+def portfolio(request):
+    image_urls = []
+    for i in range(5):
+        random_number = randint(0,100)
+        image_urls.append("https://picsum.photos/400/600/?image={}".format(random_number))
+
+    context = {'gallery_images': image_urls}
+    response = render(request, 'gallery.html', context)
+    return HttpResponse(response)
+
+def about_me(request):
+    context = {
+        'skills': ['Dependable', 'Problem solving', 'Persistent', 'Creative'],
+        'interests': ['Fitness', 'Food', 'Gunpla', 'You ;)']
+    }
+    response = render(request, 'about_me.html', context)
+    return HttpResponse(response)
+
+def favourites(request):
+    context = {
+        'fave_links': {'Bitmaker':'https://bitmaker.co/',
+        'Bitmaker Web Developement': 'https://bitmaker.co/courses/web-development',
+        'LMGTFY':'https://lmgtfy.com/'}
+    }
+    response = render(request, 'favourites.html', context)
+    return HttpResponse(response)
